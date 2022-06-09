6aa2dff8c9a3d96067114be3877b3ab78568427c
TheSachsquatch <vsachin@umich.edu>
2022-06-08 22:10:45 -0500

    Using cookie session instead of express session

--- server.js
+++ server.js
@@ -1,29 +1,24 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const session = require('express-session')
+const session = require('cookie-session')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
 //app.use(cors());
 app.use(cors({
-  origin: ["*"],
+  origin: ["http://localhost:3000", "https://routinifly.netlify.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
-
 const dbo = require("./db/conn");
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended:true}));
 app.use(session({
-    key: "userID",
+    keys: ["userID"],
     secret: process.env.COOKIE_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    cookie:{
-        expires: 60*60*24, 
-    }
+    maxAge: 60*60*24, 
 }));
 
 app.use(express.json());
